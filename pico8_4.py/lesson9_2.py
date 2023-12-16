ef49f1b00b1959380e892f1152677357462e0184
x0361392 <151415121+x0361392@users.noreply.github.com>
2023-12-16 12:56:49 +0800

    wen

---
+++ pico8_4.py/lesson9_2.py
@@ -0,0 +1,18 @@
+from machine import Timer
+
+def cllback1(t):
+    print(1)
+   
+def callbck2(t):
+    pint
+          
+time1 = timer()
+teme1.init(freq=1,calback=calback1)
+
+time2 = Timer()
+
+time2.init(period=2000,callback2)
+
+
+          
+          
