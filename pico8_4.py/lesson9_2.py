31d9bf36a4c13b895ab70cd7898b951fa55da4fa
x0361392 <151415121+x0361392@users.noreply.github.com>
2023-12-16 13:07:10 +0800

    wen

--- pico8_4.py/lesson9_2.py
+++ pico8_4.py/lesson9_2.py
@@ -1,17 +1,17 @@
 from machine import Timer
 
-def cllback1(t):
+def callback1(t):
     print(1)
-   
-def callbck2(t):
-    pint
-          
-time1 = timer()
-teme1.init(freq=1,calback=calback1)
+    
+def callback2(t):
+    print(2)
+    
+time1 = Timer()
+time1.init(freq=1,callback=callback1)
 
 time2 = Timer()
 
-time2.init(period=2000,callback2)
+time2.init(period=2000,callback=callback2)
 
 
           
